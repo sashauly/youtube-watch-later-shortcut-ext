bf62001f0079eb3d26f4be73288c857bda9b7c5c
sashauly <77851963+sashauly@users.noreply.github.com>
2025-07-16 01:04:36 +0300

    Refactor Vite configuration for consistency and add browser target option
    - Standardized quotes in vite.config.ts from double to single.
    - Added browser target option to webExtension plugin configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import { defineConfig } from 'vite';
+import webExtension, { readJsonFile } from 'vite-plugin-web-extension';
 
 function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
+  const manifest = readJsonFile('src/manifest.json');
+  const pkg = readJsonFile('package.json');
   return {
     name: pkg.name,
     description: pkg.description,
@@ -16,7 +16,8 @@ export default defineConfig({
   plugins: [
     webExtension({
       manifest: generateManifest,
-      watchFilePaths: ["package.json", "manifest.json"],
+      watchFilePaths: ['package.json', 'manifest.json'],
+      browser: process.env.TARGET || 'chrome',
     }),
   ],
 });
